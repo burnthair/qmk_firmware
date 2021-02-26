7702b6ccc9cb726dc5f738fcd196f42fa9972162
Michael Edwards <michael.edwards@gorillagroup.com>
2021-02-26 16:25:08 -0500

    _CHROME layout created, added TODOs

--- keyboards/keebio/bdn9/keymaps/burnthair/keymap.c
+++ keyboards/keebio/bdn9/keymaps/burnthair/keymap.c
@@ -17,16 +17,20 @@
 
 #define _REAPER 0
 #define _MEDIA 1
+#define _CHROME 2
 
 bool left_secondary = false;
 bool middle_secondary = false;
 bool right_secondary = false;
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
 
 enum custom_keycodes {
     LEFT_ENC = SAFE_RANGE,
     RIGHT_ENC,
     MIDDLE_ENC,
     SPOT,
+    ALT_TAB,
 };
 
 enum encoder_names {
@@ -42,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | Knob 1: Track Volume | Knob 2: Select Track | Knob 3: Vertical Scroll   |
         | Toggle Layer 1       | Home                 | End                       |
         | Arm Track (F13)      | Record (Ctrl + r)    | Play (Spacebar)           |
-     */
+    */
     [_REAPER] = LAYOUT(
         LEFT_ENC  , MIDDLE_ENC, RIGHT_ENC,
         TO(_MEDIA), KC_HOME   , KC_END   ,
@@ -56,16 +60,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | LAYOUT 1: Media
         | N/A            | N/A                            | Knob 3: Volume |
         | N/A            | N/A                            | Mute Volume    |
-        | Toggle Layer 0 | Copy Song Link (Cmd + Alt + C) | Open Spotify   |
+        | Toggle Layer 2 | Copy Song Link (Cmd + Alt + C) | Open Spotify   |
         | Media Previous | Media Play                     | Media Next     |
-     */
+    */
     [_MEDIA] = LAYOUT(
         _______    , _______         , KC_MUTE,
-        TO(_REAPER), LGUI(LALT(KC_C)), SPOT   ,
+        TO(_CHROME), LGUI(LALT(KC_C)), SPOT   ,
         KC_MPRV    , KC_MPLY         , KC_MNXT
+    ),
+    /*
+        TODO Knob 2 Browse Tabs
+        TODO Knob 3 Page Up/Down
+        TODO Super Alt Tab not working right
+        | LAYOUT 2: Chrome
+        |                | Knob 2: Browse Tabs | Knob 3: Page Up/Down |
+        |                |                     |                      |
+        | Toggle Layer 0  | Hide Window     | Cmd + Tab |
+        | Cmd + Shift + m | Cmd + Shift + i | Cmd + Shift + c |
+    */
+    [_CHROME] = LAYOUT(
+        _______    , _______   , _______   ,
+        TO(_REAPER), LGUI(KC_H), ALT_TAB   ,
+        SGUI(KC_M) , SGUI(KC_I), SGUI(KC_C)
     ),
 };
 
+/*
+    TODO rename _LEFT, _RIGHT, and _MIDDLE to their actual functions so I can add more
+    TODO fix mouse wheel interactions
+*/
 void encoder_update_user(uint8_t index, bool clockwise) {
     switch (index) {
         case _LEFT:
@@ -137,7 +160,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             _delay_ms(100);
             SEND_STRING(SS_TAP(X_ENTER));
             return false;
+        case ALT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                }
+                alt_tab_timer = timer_read();
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            return true;
         default:
             return true;
+    }
+}
+
+void matrix_scan_user(void) {
+    if (is_alt_tab_active && timer_elapsed(alt_tab_timer) > 1000) {
+        unregister_code(KC_LALT);
+        is_alt_tab_active = false;
     }
 }
