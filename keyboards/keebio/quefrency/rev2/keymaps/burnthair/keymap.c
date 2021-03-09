f6ee7e8a0467e28fc01883adcea543dda0550fe8
Michael Edwards <edwardsmichaelv@gmail.com>
2021-03-08 23:52:41 -0500

    adjust mod keys, add media keys, adjust code layout, clean up layer 2

--- keyboards/keebio/quefrency/rev2/keymaps/burnthair/keymap.c
+++ keyboards/keebio/quefrency/rev2/keymaps/burnthair/keymap.c
@@ -8,6 +8,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
+#define ENCODER_RESOLUTION 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -15,19 +16,19 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_F1,        KC_F2,        KC_GESC,      KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,      KC_EQL,       KC_BSPC,      KC_DEL,       KC_HOME,      \
-    KC_F3,        KC_F4,        KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_LBRC,      KC_RBRC,      KC_BSLS,      KC_END,      \
-    KC_F5,        KC_F6,        KC_CAPS,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      KC_QUOT,      KC_ENT,       KC_PGUP,      \
-    KC_F7,        KC_F8,        KC_LSFT,      LCTL(KC_LSFT),KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT,      KC_UP,        KC_PGDN,      \
-    KC_F9,        KC_F10,       MO(_FN1),     KC_LCTL,      KC_LGUI,      KC_LALT,      KC_SPC,                     KC_ENT,       KC_BSPC,      KC_RALT,      KC_RCTL,      MO(_FN1),     KC_LEFT,      KC_DOWN,      KC_RGHT
+    KC_F1, KC_GRAVE,     KC_ESC,       KC_1,   KC_2,   KC_3,    KC_4,  KC_5,KC_6,  /**/  KC_7,  KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_DEL, KC_MUTE,\
+    KC_F1, KC_F2,        KC_TAB,       KC_Q,   KC_W,   KC_E,    KC_R,  KC_T,       /**/  KC_Y,  KC_U,   KC_I,   KC_O,   KC_P,    KC_LBRC,KC_RBRC,KC_BSLS,KC_PGUP,\
+    KC_F3, KC_F4,        KC_LCTL,      KC_A,   KC_S,   KC_D,    KC_F,  KC_G,       /**/  KC_H,  KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,KC_ENT,         KC_PGDN,\
+    KC_F5, KC_F6,        LCTL(KC_LSFT),KC_LSFT,KC_Z,   KC_X,    KC_C,  KC_V,KC_B,  /**/  KC_N,  KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,        KC_UP,  KC_END ,\
+    KC_F7, KC_F8,        KC_LCTL      ,KC_LGUI,KC_LALT,MO(_FN1),KC_SPC,            /**/  KC_SPC,KC_BSPC,        KC_RALT,KC_RCTL,MO(_FN1),KC_LEFT,KC_DOWN,KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
-    _______, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_DEL,  _______, \
-    _______, _______, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+    KC_F9  , KC_F10 , RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    KC_F11 , KC_F12 , _______, _______, KC_UP,   _______, _______, _______,          /**/ _______, _______, _______, _______, _______, _______, _______, _______, KC_END, \
+    _______, _______, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          /**/ _______, _______, _______, _______, _______, _______, _______,          _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,          _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,                   /**/ _______, _______,          _______, _______, _______, KC_HOME, _______, KC_END
   )
 };
 
