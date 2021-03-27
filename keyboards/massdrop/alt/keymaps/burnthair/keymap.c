a9351ec10e30c0cf5d7783e360674a3ee1e1df83
MVE <edwardsmichaelv@gmail.com>
2021-03-27 18:46:24 -0400

    add Windows mode for Alt

--- keyboards/massdrop/alt/keymaps/burnthair/keymap.c
+++ keyboards/massdrop/alt/keymaps/burnthair/keymap.c
@@ -1,16 +1,32 @@
 #include QMK_KEYBOARD_H
 
+#define _MAC 0
+#define _FN1 1
+#define _WIN 2
+
 // Left-hand home row mods (Mac CAGS)
-#define HOME_A LCTL_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
+#define MAC_A LCTL_T(KC_A)
+#define MAC_S LALT_T(KC_S)
+#define MAC_D LGUI_T(KC_D)
+#define MAC_F LSFT_T(KC_F)
 
 // Right-hand home row mods (Mac CAGS)
-#define HOME_J RSFT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RCTL_T(KC_SCLN)
+#define MAC_J RSFT_T(KC_J)
+#define MAC_K RGUI_T(KC_K)
+#define MAC_L LALT_T(KC_L)
+#define MAC_SCLN RCTL_T(KC_SCLN)
+
+// Left-hand home row mods (Win GACS)
+#define WIN_A LGUI_T(KC_A)
+#define WIN_S LALT_T(KC_S)
+#define WIN_D LCTL_T(KC_D)
+#define WIN_F LSFT_T(KC_F)
+
+// Right-hand home row mods (Win GACS)
+#define WIN_J RSFT_T(KC_J)
+#define WIN_K RCTL_T(KC_K)
+#define WIN_L LALT_T(KC_L)
+#define WIN_SCLN RGUI_T(KC_SCLN)
 
 enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
@@ -23,29 +39,27 @@ enum alt_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi_blocker(
+    [_MAC] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP, \
-        KC_GRV,  HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,KC_QUOT,         KC_ENT,  KC_PGDN, \
+        KC_GRV,  MAC_A,   MAC_S,   MAC_D,   MAC_F,   KC_G,    KC_H,    MAC_J,   MAC_K,   MAC_L,   MAC_SCLN,KC_QUOT,          KC_ENT,  KC_PGDN, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,  \
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
-    [1] = LAYOUT_65_ansi_blocker(
+    [_FN1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_VOLU, \
         KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLD, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, KC_MPRV, KC_MNXT, KC_MPLY, _______,          KC_PGUP, KC_HOME, \
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, KC_MPRV, KC_MNXT, KC_MPLY, TG(_WIN),         KC_PGUP, KC_HOME, \
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END   \
     ),
-    /*
-    [X] = LAYOUT(
+    [_WIN] = LAYOUT_65_ansi_blocker(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, WIN_A,   WIN_S,   WIN_D,   WIN_F,   _______, _______, WIN_J,   WIN_K,   WIN_L,   WIN_SCLN,_______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
+        KC_LCTL, KC_LGUI, KC_LALT,                            _______,                            KC_RALT, _______, _______, _______, _______  \
     ),
-    */
 };
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
